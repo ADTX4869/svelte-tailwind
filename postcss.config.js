1a517952a5059e7bc9ac7d4a54557c5eb1414294
Anyyz <ADTX-4869@outlook.com>
2020-07-15 10:56:26 +0000

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+const production = !process.env.ROLLUP_WATCH;
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
+module.exports = {
+    plugins: [
+        require("tailwindcss"),
+        cssnano({
+            autoprefixer: false,
+            preset: ["default"],
+        }),
+        production && purgecss({
+            content: ["./**/*.html", "./src/**/*.svelte"],
+            defaultExtractor: content = content.match(/[A-Za-z0-9-_:/]+/g || [])
+        })
+    ]
+}
